814748c002db90733f3e20c1a38275f398d98c64
Viacheslav Pliusnin <via.plsn@gmail.com>
2022-09-23 18:49:53 +0300

    Prettier test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,6 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html><Head /><body><Main /><NextScript /></body></Html>
+    <Html><Head><title>Test</title></Head><body><Main /><NextScript /></body></Html>
   );
 }
