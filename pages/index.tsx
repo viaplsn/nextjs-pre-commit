17a2bc6f646c8491f153c225db77d29f6c990564
Viacheslav Pliusnin <via.plsn@gmail.com>
2022-09-23 18:25:08 +0300

    Test commit with warnings

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const neverUsed = "";
+
   return (
     <div className={styles.container}>
       <Head>
